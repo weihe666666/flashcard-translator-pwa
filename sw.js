7de634266b36d1f339ae8277edbeb663e1e6758f
weihe666666 <wei.he@arup.com>
2024-08-25 20:57:45 +1000

    images

--- sw.js
+++ sw.js
@@ -1,24 +1,28 @@
-// sw.js (Service Worker)
+const cacheName = 'chat-app-v1';
+const assetsToCache = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/app.js',
+    '/manifest.json',
+    '/splash_screens/icon.png',
+    '/splash_screens/icon.png'
+];
 
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open('flashcard-translator-cache').then(cache => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/styles.css',
-                '/app.js',
-                '/manifest.json',
-                '/icon.png'
-            ]);
-        })
+        caches.open(cacheName)
+            .then(cache => {
+                return cache.addAll(assetsToCache);
+            })
     );
 });
 
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
     );
 });
